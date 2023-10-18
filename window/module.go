0a615a0323b5d50ab286e54f47918329d47af30a
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2023-10-18 10:39:54 -0700

    add additional window module methods

--- window/module.go
+++ window/module.go
@@ -5,6 +5,7 @@ import (
 	"ghostlang.org/x/ghost/object"
 	"ghostlang.org/x/ghost/token"
 	"github.com/shopspring/decimal"
+	"github.com/veandco/go-sdl2/sdl"
 )
 
 var WindowMethods = map[string]*object.LibraryFunction{}
@@ -12,6 +13,10 @@ var WindowProperties = map[string]*object.LibraryProperty{}
 
 func init() {
 	modules.RegisterMethod(WindowMethods, "title", windowTitle)
+	modules.RegisterMethod(WindowMethods, "bordered", windowsBordered)
+	modules.RegisterMethod(WindowMethods, "borderless", windowsBorderless)
+	modules.RegisterMethod(WindowMethods, "fullscreen", windowsFullscreen)
+	modules.RegisterMethod(WindowMethods, "resize", windowsResize)
 
 	modules.RegisterProperty(WindowProperties, "title", windowTitleProperty)
 	modules.RegisterProperty(WindowProperties, "width", windowWidthProperty)
@@ -25,6 +30,38 @@ func windowTitle(scope *object.Scope, tok token.Token, args ...object.Object) ob
 	}
 
 	Window.Window.SetTitle(args[0].(*object.String).Value)
+
+	return nil
+}
+
+func windowsBordered(scope *object.Scope, tok token.Token, args ...object.Object) object.Object {
+	Window.Window.SetBordered(true)
+
+	return nil
+}
+
+func windowsBorderless(scope *object.Scope, tok token.Token, args ...object.Object) object.Object {
+	Window.Window.SetBordered(false)
+
+	return nil
+}
+
+func windowsFullscreen(scope *object.Scope, tok token.Token, args ...object.Object) object.Object {
+	Window.Window.SetFullscreen(sdl.WINDOW_FULLSCREEN)
+
+	return nil
+}
+
+func windowsResize(scope *object.Scope, tok token.Token, args ...object.Object) object.Object {
+	if len(args) != 2 {
+		panic("wrong number of arguments. expected=2")
+		// return object.NewError("wrong number of arguments. got=%d, expected=2", len(args))
+	}
+
+	width := args[0].(*object.Number).Value.IntPart()
+	height := args[1].(*object.Number).Value.IntPart()
+
+	Window.Window.SetSize(int32(width), int32(height))
 
 	return nil
 }
