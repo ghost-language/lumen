dfb55e815dcc1df012651df79d197128d110d506
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2023-10-17 21:47:19 -0700

    add graphics.circle

--- graphic/module.go
+++ graphic/module.go
@@ -1,6 +1,8 @@
 package graphic
 
 import (
+	"math"
+
 	"ghostlang.org/x/ghost/library/modules"
 	"ghostlang.org/x/ghost/object"
 	"ghostlang.org/x/ghost/token"
@@ -17,6 +19,7 @@ func init() {
 	modules.RegisterMethod(GraphicsMethods, "setColor", graphicsSetColor)
 	modules.RegisterMethod(GraphicsMethods, "rectangle", graphicsRectangle)
 	modules.RegisterMethod(GraphicsMethods, "filledRectangle", graphicsFilledRectangle)
+	modules.RegisterMethod(GraphicsMethods, "circle", graphicsCircle)
 	modules.RegisterMethod(GraphicsMethods, "line", graphicsLine)
 	modules.RegisterMethod(GraphicsMethods, "clear", graphicsClear)
 	modules.RegisterMethod(GraphicsMethods, "point", graphicsPoint)
@@ -89,6 +92,43 @@ func graphicsFilledRectangle(scope *object.Scope, tok token.Token, args ...objec
 	rectangle := sdl.Rect{X: x, Y: y, W: w, H: h}
 
 	renderer.Renderer.FillRect(&rectangle)
+
+	return nil
+}
+
+func graphicsCircle(scope *object.Scope, tok token.Token, args ...object.Object) object.Object {
+	if len(args) != 3 {
+		panic("wrong number of arguments. expected=3")
+		// return object.NewError("wrong number of arguments. got=%d, expected=3", len(args))
+	}
+
+	x := int32(args[0].(*object.Number).Value.IntPart())
+	y := int32(args[1].(*object.Number).Value.IntPart())
+	r := int32(args[2].(*object.Number).Value.IntPart())
+
+	d := int32(math.Pi - float64(2*r))
+
+	x0 := int32(0)
+	y0 := int32(r)
+
+	for x0 <= y0 {
+		renderer.Renderer.DrawPoint(x+x0, y+y0)
+		renderer.Renderer.DrawPoint(x-x0, y-y0)
+		renderer.Renderer.DrawPoint(x-x0, y+y0)
+		renderer.Renderer.DrawPoint(x+x0, y-y0)
+		renderer.Renderer.DrawPoint(x+y0, y+x0)
+		renderer.Renderer.DrawPoint(x-y0, y-x0)
+		renderer.Renderer.DrawPoint(x-y0, y+x0)
+		renderer.Renderer.DrawPoint(x+y0, y-x0)
+
+		if d < 0 {
+			d = d + int32((math.Pi*float64(x0))+(math.Pi*2))
+		} else {
+			d = d + int32((math.Pi*float64(x0-y0))+(math.Pi*3))
+			y0--
+		}
+		x0++
+	}
 
 	return nil
 }
