3dab7b5ce719e31d41cb14c9bb1f1071e95daa6a
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2023-10-22 17:11:59 -0700

    add color.rgb and color.hex methods

--- color/module.go
+++ color/module.go
@@ -1,6 +1,9 @@
 package color
 
 import (
+	"strconv"
+	"strings"
+
 	"ghostlang.org/x/ghost/library/modules"
 	"ghostlang.org/x/ghost/object"
 	"ghostlang.org/x/ghost/token"
@@ -10,7 +13,8 @@ var ColorMethods = map[string]*object.LibraryFunction{}
 var ColorProperties = map[string]*object.LibraryProperty{}
 
 func init() {
-	// modules.RegisterMethod(ColorMethods, "isDown", keyboardIsDown)
+	modules.RegisterMethod(ColorMethods, "rgb", colorRgbMethod)
+	modules.RegisterMethod(ColorMethods, "hex", colorHexMethod)
 
 	modules.RegisterProperty(ColorProperties, "black", colorBlackProperty)
 	modules.RegisterProperty(ColorProperties, "darkBlue", colorDarkBlueProperty)
@@ -28,6 +32,87 @@ func init() {
 	modules.RegisterProperty(ColorProperties, "lavender", colorLavenderProperty)
 	modules.RegisterProperty(ColorProperties, "pink", colorPinkProperty)
 	modules.RegisterProperty(ColorProperties, "peach", colorPeachProperty)
+}
+
+func colorRgbMethod(scope *object.Scope, tok token.Token, args ...object.Object) object.Object {
+	if len(args) < 3 {
+		panic("wrong number of arguments. expected=3 or 4")
+		// return object.NewError("wrong number of arguments. got=%d, expected=2", len(args))
+	}
+
+	red := uint8(args[0].(*object.Number).Value.IntPart())
+	green := uint8(args[1].(*object.Number).Value.IntPart())
+	blue := uint8(args[2].(*object.Number).Value.IntPart())
+	alpha := uint8(255)
+
+	if len(args) == 4 {
+		alpha = uint8(args[3].(*object.Number).Value.IntPart())
+	}
+
+	color := new(Color)
+
+	color.Red = red
+	color.Green = green
+	color.Blue = blue
+	color.Alpha = alpha
+
+	return color
+}
+
+func colorHexMethod(scope *object.Scope, tok token.Token, args ...object.Object) object.Object {
+	if len(args) < 1 {
+		panic("wrong number of arguments. expected=1")
+		// return object.NewError("wrong number of arguments. got=%d, expected=1", len(args))
+	}
+
+	hex := args[0].(*object.String).Value
+
+	hex = strings.TrimPrefix(hex, "#")
+
+	var red, green, blue, alpha uint8
+
+	// Default alpha value
+	alpha = 255
+
+	if len(hex) == 3 {
+		r, _ := strconv.ParseInt(string(hex[0])+string(hex[0]), 16, 32)
+		g, _ := strconv.ParseInt(string(hex[1])+string(hex[1]), 16, 32)
+		b, _ := strconv.ParseInt(string(hex[2])+string(hex[2]), 16, 32)
+
+		red = uint8(r)
+		green = uint8(g)
+		blue = uint8(b)
+	} else if len(hex) == 6 {
+		r, _ := strconv.ParseInt(hex[0:2], 16, 32)
+		g, _ := strconv.ParseInt(hex[2:4], 16, 32)
+		b, _ := strconv.ParseInt(hex[4:6], 16, 32)
+
+		red = uint8(r)
+		green = uint8(g)
+		blue = uint8(b)
+	} else if len(hex) == 8 {
+		r, _ := strconv.ParseInt(hex[0:2], 16, 32)
+		g, _ := strconv.ParseInt(hex[2:4], 16, 32)
+		b, _ := strconv.ParseInt(hex[4:6], 16, 32)
+		a, _ := strconv.ParseInt(hex[6:8], 16, 32)
+
+		red = uint8(r)
+		green = uint8(g)
+		blue = uint8(b)
+		alpha = uint8(a)
+	} else {
+		panic("invalid hex value")
+		// return object.NewError("invalid hex value")
+	}
+
+	color := new(Color)
+
+	color.Red = red
+	color.Green = green
+	color.Blue = blue
+	color.Alpha = alpha
+
+	return color
 }
 
 func colorBlackProperty(scope *object.Scope, tok token.Token) object.Object {
