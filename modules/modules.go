6959ce8fd2d887c039855d12653aaa4f002a677b
Shea Lewis (Kai) <355659+kaidesu@users.noreply.github.com>
2023-10-29 20:46:32 -0700

    add keyboard module

--- modules/modules.go
+++ modules/modules.go
@@ -5,5 +5,6 @@ import "ghostlang.org/x/ghost/ghost"
 func Register() {
 	ghost.RegisterModule("canvas", CanvasMethods, CanvasProperties)
 	ghost.RegisterModule("color", ColorMethods, ColorProperties)
+	ghost.RegisterModule("keyboard", KeyboardMethods, KeyboardProperties)
 	ghost.RegisterModule("window", WindowMethods, WindowProperties)
 }
